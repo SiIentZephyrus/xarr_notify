e66a8a662c219832df1c5742a929d4e45da5f873
SilentZephyrus <493943180@qq.com>
2022-03-21 17:58:23 +0800

    build: 准备自动构建docker镜像

---
+++ build/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+apt update
+apt install -y cron libsasl2-dev python-dev libldap2-dev libssl-dev
+pip install --upgrade pip
+pip install -r /var/requirements.txt
+chmod -R 777 /var/startup.sh
