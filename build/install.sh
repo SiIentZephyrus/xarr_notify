ed090814d0144ae63f477133f5c9eb44a4d13af1
SilentZephyrus <493943180@qq.com>
2022-03-22 14:57:16 +0800

    build: 修复构建错误

--- build/install.sh
+++ build/install.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 set -e
-apt update
-apt install -y cron libsasl2-dev python-dev libldap2-dev libssl-dev
 pip install --upgrade pip
 pip install -r ../requirements.txt
 chmod -R 777 ./run.sh
